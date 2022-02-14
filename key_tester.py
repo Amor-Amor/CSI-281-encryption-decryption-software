8c1d8a4277bdc22c4ee881b8f1ceb41f8fd9c294
Charles Gagnon <charles.gagnon@mymail.champlain.edu>
2022-02-14 17:35:45 -0500

    Added simple skeleton for testing various types of keys.

---
+++ key_tester.py
@@ -0,0 +1,28 @@
+import random
+
+
+class KeyObj:
+    def __init__(self, key):
+        self.key = key
+
+    def algorithm_caesar(self):
+        """Uses caesar cipher to perform encryption"""
+        # TODO
+
+    def encode(self, message):
+        """Encodes a message based on the desired key"""
+        # TODO
+
+    def decode(self, encoded_message):
+        """Decodes the message based on the key that initialized the object"""
+        # TODO
+
+
+def main():
+    print("Enter desired key: ")
+    key = input()
+    KeyObj(key)
+
+
+if __name__ == "__main__":
+    main()
