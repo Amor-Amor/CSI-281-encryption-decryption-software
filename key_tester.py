0b378a559a3705c5f9e34db3dd6978ab7af197e8
Charles Gagnon <80009093+charlesgagnonChamplain@users.noreply.github.com>
2022-02-14 19:16:25 -0500

    Added various styles of key generation

--- key_tester.py
+++ key_tester.py
@@ -2,9 +2,39 @@ import random
 
 
 class KeyObj:
-    def __init__(self, key):
+    def __init__(self, key, style):
         self.key = key
+        self.style = style
 
+    def string_const_key()
+        """Takes in key based solely on input as a single string"""
+        # TODO
+        
+     def array_const_key()
+        """Takes in key based solely on input as an array"""
+        # TODO
+        
+    def string_rand_key()
+        """Generates a random key as a single string"""
+        # TODO
+        
+     def array_rand_key()
+        """Generates random key as an array"""
+        # TODO
+     
+    # https://www.ssh.com/academy/ssh/keygen
+    def rsa()
+        """Uses standard rsa key style"""
+        # TODO
+        
+    def dsa()
+        """Uses standard rsa key style"""
+        # TODO
+    
+    def ecdsa()
+        """Uses standard rsa key style"""
+        # TODO
+    
     def alg_caesar(self):
         """Uses caesar cipher to perform encryption"""
         # TODO
@@ -21,7 +51,7 @@ class KeyObj:
 def main():
     print("Enter message: ")
     message = input()
-    print("Enter algorithm style: ")
+    print("Enter key style: ")
     alg = input()
     print("Enter desired key: ")
     key = input()
