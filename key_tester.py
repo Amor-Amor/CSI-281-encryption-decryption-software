528de591cf7077897ddc0be3f0d2bb954be0c937
Charles Gagnon <charles.gagnon@mymail.champlain.edu>
2022-02-20 19:21:34 -0500

    WIP Permutation key

--- key_tester.py
+++ key_tester.py
@@ -1,61 +1,74 @@
 import random
 
 
-class KeyObj:
-    def __init__(self, key, style):
-        self.key = key
-        self.style = style
+def permutation_key():
+    """Generate key for permutation cypher"""
+    size = random.randint(3, 10)
+    key = []
+    count = 0
+    for i in range(0, size, 1):
+        i = count
+        key.append(i)
+        count += 1
+    print(key)
+    # TODO random shuffle
 
-    def string_const_key(self):
-        """Takes in key based solely on input as a single string"""
-        # TODO
+    return key
 
-    def array_const_key(self):
-        """Takes in key based solely on input as an array"""
-        # TODO
 
-    def string_rand_key(self):
-        """Generates a random key as a single string"""
-        # TODO
+def string_const_key():
+    """Takes in key based solely on input as a single string"""
+    # TODO
 
-    def array_rand_key(self):
-        """Generates random key as an array"""
-        # TODO
 
-    # https://www.ssh.com/academy/ssh/keygen
-    def rsa(self):
-        """Uses standard rsa key style"""
-        # TODO
+def array_const_key():
+    """Takes in key based solely on input as an array"""
+    # TODO
 
-    def dsa(self):
-        """Uses standard dsa key style"""
-        # TODO
 
-    def ecdsa(self):
-        """Uses standard ecdsa key style"""
-        # TODO
+def string_rand_key():
+    """Generates a random key as a single string"""
+    # TODO
 
-    def alg_caesar(self):
-        """Uses caesar cipher to perform encryption"""
-        # TODO
 
-    def encode(self, message):
-        """Encodes a message based on the desired key"""
-        # TODO
+def array_rand_key():
+    """Generates random key as an array"""
+    # TODO
 
-    def decode(self, encoded_message):
-        """Decodes the message based on the key that initialized the object"""
-        # TODO
+
+# https://www.ssh.com/academy/ssh/keygen
+def rsa():
+    """Uses standard rsa key style"""
+    # TODO
+
+
+def dsa():
+    """Uses standard dsa key style"""
+    # TODO
+
+
+def ecdsa():
+    """Uses standard ecdsa key style"""
+    # TODO
+
+
+def alg_caesar():
+    """Uses caesar cipher to perform encryption"""
+    # TODO
+
+
+def encode(message):
+    """Encodes a message based on the desired key"""
+    # TODO
+
+
+def decode(encoded_message):
+    """Decodes the message based on the key that initialized the object"""
+    # TODO
 
 
 def main():
-    print("Enter message: ")
-    message = input()
-    print("Enter key style: ")
-    alg = input()
-    print("Enter desired key: ")
-    key = input()
-    KeyObj(key)
+    key = permutation_key()
 
 
 if __name__ == "__main__":
