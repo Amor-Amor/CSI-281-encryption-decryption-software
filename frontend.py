e7efcddb15a38b04cf1ad3c78968bec035aab16d
hhennessy20 <hhennessy20@gmail.com>
2022-02-28 16:52:16 -0500

    Update frontend.py
    Switched from camel case to snake case

--- frontend.py
+++ frontend.py
@@ -33,29 +33,29 @@ close_button= Button(top_bar, text="X", bg='grey15', fg='grey80', command=m.dest
 main_window= Canvas(m, bg='grey10')
 
 # Selection box for user to select encryption algorithm
-algorithmSelectorLabel = Label(main_window, text='Select Algorithm: ', font=('courier', 8), bg='grey10', fg='grey80').grid(row=1, column=0, columnspan=1, padx=10, pady=10)
+algorithm_selector_label = Label(main_window, text='Select Algorithm: ', font=('courier', 8), bg='grey10', fg='grey80').grid(row=1, column=0, columnspan=1, padx=10, pady=10)
 algorithms = ['Caesar', 'None']
-selectedAlgorithm = StringVar(m)
-selectedAlgorithm.set('Caesar')
-selectionBox = OptionMenu(main_window, selectedAlgorithm, *algorithms)
-selectionBox.config(bg= 'grey15', fg='grey80', font=('courier', 8))
-selection_box_menu = main_window.nametowidget(selectionBox.menuname)
+selected_algorithm = StringVar(m)
+selected_algorithm.set('Caesar')
+selection_box = OptionMenu(main_window, selected_algorithm, *algorithms)
+selection_box.config(bg= 'grey15', fg='grey80', font=('courier', 8))
+selection_box_menu = main_window.nametowidget(selection_box.menuname)
 selection_box_menu.config(font=('courier', 8))
-selectionBox.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
+selection_box.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
 # Text and key entry fields
-textEntryLabel = Label(main_window, text='Enter Text: ', bg='grey10', fg='grey80', font=('courier', 8)).grid(row=2, column=0, columnspan=1, padx=5, pady=5)
-textEntry = tk.Entry(main_window, textvariable=in_box, font=('courier', 8), bg='grey15', fg='grey80').grid(row=2, column=1, padx=5, pady=5)
-keyEntryLabel = Label(main_window, text='Key: ', bg='grey10', fg='grey80', font=('courier', 8)).grid(row=2, column=2, columnspan=1, padx=5, pady=5)
-keyEntry = Text(main_window, height=1, width=5, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=2, column=3, padx=5, pady=5)
+text_entry_label = Label(main_window, text='Enter Text: ', bg='grey10', fg='grey80', font=('courier', 8)).grid(row=2, column=0, columnspan=1, padx=5, pady=5)
+text_entry = tk.Entry(main_window, textvariable=in_box, font=('courier', 8), bg='grey15', fg='grey80').grid(row=2, column=1, padx=5, pady=5)
+key_entry_label = Label(main_window, text='Key: ', bg='grey10', fg='grey80', font=('courier', 8)).grid(row=2, column=2, columnspan=1, padx=5, pady=5)
+key_entry = Text(main_window, height=1, width=5, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=2, column=3, padx=5, pady=5)
 
 # Encrypt and Decrypt Buttons
-encryptButton = Button(main_window, text='Encrypt', command=click_encrypt_button, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=3, column=1, padx=10, pady=10)
-decryptButton = Button(main_window, text='Decrypt', command=click_decrypt_button, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=3, column=2, padx=10, pady=10)
+encrypt_button = Button(main_window, text='Encrypt', command=click_encrypt_button, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=3, column=1, padx=10, pady=10)
+decrypt_button = Button(main_window, text='Decrypt', command=click_decrypt_button, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=3, column=2, padx=10, pady=10)
 
 # Output Label and Box
-outputLabel = Label(main_window, text="Output: ",bg='grey10', fg='grey80', font=('courier', 8)).grid(row=4, column=0, padx=10, pady=10)
-outputBox = Text(main_window, height=1, width=40, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=4, column=1, columnspan=2, padx=10, pady=10)
+output_label = Label(main_window, text="Output: ",bg='grey10', fg='grey80', font=('courier', 8)).grid(row=4, column=0, padx=10, pady=10)
+output_box = Text(main_window, height=1, width=40, bg='grey15', fg='grey80', font=('courier', 8)).grid(row=4, column=1, columnspan=2, padx=10, pady=10)
 
 # Sets window geometry and title bar
 m.overrideredirect(True)
@@ -64,6 +64,7 @@ top_bar.pack(expand=1, fill=X)
 top_bar_title.pack(side=LEFT)
 close_button.pack(side=RIGHT)
 main_window.pack(expand=1, fill=BOTH)
+top_bar_title.bind('<B1-Motion>', move_window)
 top_bar.bind('<B1-Motion>', move_window)
 
 #runs GUI
